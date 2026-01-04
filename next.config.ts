290cc120fb648885eab1130a811edcc430863905
AnthonyDelgadoMiami <anthony8097@gmail.com>
2026-01-03 20:35:49 -0500

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // <-- This is the key line
+  // Optional: Add this if you use the Next.js Image component
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
